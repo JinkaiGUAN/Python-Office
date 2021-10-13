00a13e9670e080984bd737e3b4aeb3ce4eb5f558
JinkaiGUAN <pg121@ic.ac.uk>
2021-10-13 12:14:41 +0100

    linkedlist-based queue

--- 2-LinkedList/singly_linkedlist.py
+++ 2-LinkedList/singly_linkedlist.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Union
 
 
 class Node:
@@ -9,7 +9,7 @@ class Node:
         self.next = next
 
 
-class LinkedStack:
+class LinkedListStack:
     """LIFO Stack implementation using a singly linked list for storage."""
 
     def __init__(self, value: Any = None) -> None:
@@ -56,3 +56,58 @@ class LinkedStack:
 
         return answer
         
+
+class LinkedListQueue:
+    def __init__(self, value: Union[float, int, str]) -> None:
+        """FIFO queue implementation using a singly linked list for storage."""
+        if value:
+            self.head = self.tail = Node(value)
+            self.size = 1
+
+        self.head = None
+        self.tail = None
+        self.size  = 0
+
+    def __len__(self) -> int:
+        return self.size
+
+    def is_empty(self) -> bool:
+        return self.size == 0
+
+    def first(self) -> Union[float, int, str]:
+        if self.is_empty():
+            raise Exception('The queue is empty!')
+        
+        return self.head.value
+
+    def dequeue(self) -> Union[float, int, str]:
+        """Remove and return the first element of the queue (i.e., FIFO).
+
+        Raises:
+            Exception: Raise exception is the queue is empty.
+
+        Returns:
+            Union[float, int, str]: [description]
+        """
+        if self.is_empty():
+            raise Exception('The queue is empty!')
+
+        answer = self.head.value
+        self.head = self.head.next
+        self.size -= 1
+        if self.is_empty():
+            self.tail = None
+        
+        return answer
+
+    def enqueue(self, value: Union[float, int, str]) -> None:
+        new_node = None(value)
+        if self.is_empty():
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+        self.tail = new_node
+        
+        self.size += 1
+
+

98967c514ae2832390a2898484301043f7ff8d13
JinkaiGUAN <pg121@ic.ac.uk>
2021-12-04 07:15:43 +0000

    using threading to block I/O

---
+++ Threads/note.md
@@ -0,0 +1,17 @@
+
+## Two ways run a Python file based on CPython
+- CPython parses and compiles the source text into bytecode.
+- CPython runs the bytecode using a stack-based interpreter.
+    -  CPython uses GIL to main coherence when running the programme.
+    
+## Needs to remember
+- Python threads cannot run in parallel on multiple CPU cores because of the global interpreter lock.
+- Python threads are still useful despite the GIL because they provide an easy way to do multiple things seemingly 
+  at the same time.
+  
+- Use Python threads to make multiple system calls in parallel. This allows you to do blocking I/O at the same time 
+  as computation.
+  
+## Questions:
+- What's the thread?
+- Using thread to complete the parallel task is reasonable or not?
--- Threads/proxy.py
+++ Threads/proxy.py
@@ -7,3 +7,40 @@
 @Date    : 04/12/2021 06:32 
 @Brief   : 
 """
+import typing as t
+import time
+from threading import Thread
+
+
+def factorize(number: int) -> t.Generator:
+    for i in range(1, number + 1):
+        if number % i == 0:
+            yield i
+
+
+class FactorizeThread(Thread):
+    def __init__(self, number: int):
+        super(FactorizeThread, self).__init__()
+        self.num = number
+
+    def run(self):
+        self.factors = list(factorize(self.num))
+
+
+if __name__ == '__main__':
+    # However, this kind of thread would not speed up this kind of problem compared to other languagaes due to GIL.
+    start = time.time()
+
+    threads = []
+    numbers = [2139079, 1214759, 1516637, 1852285]
+    for num in numbers:
+        thread = FactorizeThread(num)
+        thread.start()
+        threads.append(thread)
+
+    # wait for all of the threads to finish
+    for thread in threads:
+        thread.join()
+
+    end = time.time()
+    print(f'Time used is {end - start}')
